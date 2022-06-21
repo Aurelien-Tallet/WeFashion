3e9b723b977f31f66f59257eb3f523ebd30417bd
Aurelien-Tallet <aurelien.jarte@gmail.com>
2022-06-21 12:11:08 +0200

    data structure setup

---
+++ database/migrations/2022_06_21_085841_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('description')->nullable();
+            $table->float('price', 8, 2);
+            $table->enum('status', ['publish', 'unpublish'])->default('unpublish');
+            $table->string('reference', 16);
+            $table->enum('discount', ['discount', 'standard'])->default('standard');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
